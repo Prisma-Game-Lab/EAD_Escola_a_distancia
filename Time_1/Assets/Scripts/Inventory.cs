d825f13ece3e631240af960bb5a1af323702d28d
Nagib Moura Suaid <nagibmoura@gmail.com>
2021-10-08 15:29:44 -0300

    Prep for item interactions

--- Time_1/Assets/Scripts/Inventory.cs
+++ Time_1/Assets/Scripts/Inventory.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 [CreateAssetMenu(fileName = "Inventory", menuName = "Item/Create a new Item Inventory")]
 public class Inventory : ScriptableObject
 {
     public List<Item> itensList = new List<Item>();
+
+    public void RemoveItem(Item i)
+    {
+        Assert.IsTrue(itensList.Contains(i));
+        itensList.Remove(i);
+    }
+
+    public void AddItem(Item i)
+    {
+        itensList.Add(i);
+    }
 
 }
