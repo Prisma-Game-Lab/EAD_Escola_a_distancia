12429572de11c313508d6580e488f0a32312ce1f
Thomaz Miranda <thomazrebellomiranda@gmail.com>
2021-11-22 19:38:47 -0300

    Camera Follow

--- Time_1/Assets/Scripts/CameraController.cs
+++ Time_1/Assets/Scripts/CameraController.cs
@@ -5,28 +5,22 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject targetObject;
-    private float targetAngle = 0;
-    [SerializeField] private float rotationAmount = 1f;
     [SerializeField] private float scrollIntensity;
     [SerializeField] private float minDist;
     [SerializeField] private float maxDist;
     private Camera cam;
+    private Vector3 offset;
 
+    void Awake()
+    {
+        offset = transform.position - targetObject.transform.position;
+    }
     void Start()
     {
         cam = gameObject.GetComponent<Camera>();
     }
     void Update()
     {
-        if (Input.GetButtonDown("RotateCameraLeft")) 
-        {
-            Left();
-        } 
-        else if (Input.GetButtonDown("RotateCameraRight")) 
-        {
-            Right();
-        }
-
         cam.orthographicSize -= Input.GetAxisRaw("Mouse ScrollWheel") * scrollIntensity;
         if (cam.orthographicSize < minDist)
             cam.orthographicSize = minDist;
@@ -35,36 +29,8 @@ public class CameraController : MonoBehaviour
             cam.orthographicSize = maxDist;
     }
 
-    void FixedUpdate() 
+    void LateUpdate()
     {
-        if(targetAngle !=0)
-        {
-            Rotate();
-        }      
-    }
-
-    public void Right()
-    {
-        targetAngle += 90.0f;
-    }
-
-    public void Left()
-    {
-        targetAngle -= 90.0f;
-    }
-
-
-    protected void Rotate()
-    {
-        if (targetAngle>0)
-        {
-            transform.RotateAround(targetObject.transform.position, Vector3.up, -rotationAmount);
-            targetAngle -= rotationAmount;
-        }
-        else if(targetAngle <0)
-        {
-            transform.RotateAround(targetObject.transform.position, Vector3.up, rotationAmount);
-            targetAngle += rotationAmount;
-        }
+        transform.position = targetObject.transform.position + offset;
     }
 }
