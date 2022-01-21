bbd4825f3706c5233accee1cc984b118f09371e6
Nagib Moura Suaid <nagibmoura@gmail.com>
2022-01-21 13:15:17 -0300

    Mudei prefab de armario no hub

---
+++ Time_1/Assets/Scripts/MassReplace.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+// CopyComponents - by Michael L. Croswell for Colorado Game Coders, LLC
+// March 2010
+
+public class ReplaceGameObjects : ScriptableWizard
+{
+    public bool copyValues = true;
+    public GameObject prefab;
+    public GameObject[] objects;
+
+
+    [MenuItem("Custom/Replace GameObjects")]
+
+
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard("Replace GameObjects", typeof(ReplaceGameObjects), "Replace");
+    }
+
+    void OnWizardCreate()
+    {
+
+        foreach (GameObject go in objects)
+        {
+            Transform t = go.transform;
+            GameObject newObject;
+            newObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+            newObject.transform.position = t.position;
+            newObject.transform.rotation = t.rotation;
+            newObject.transform.parent = t.parent;
+
+            DestroyImmediate(go);
+
+        }
+
+    }
+}
