a5b9012531a1854022c147de8b7897f0a73f469d
Thomaz Miranda <thomazrebellomiranda@gmail.com>
2022-01-21 13:32:44 -0300

    puzzle informatica

---
+++ Time_1/Assets/Scripts/InfPuzzle.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
+
+public class InfPuzzle : MonoBehaviour
+{
+    public string password;
+    public string playerText;
+    public TextMeshProUGUI blinkText;
+    public TextMeshProUGUI mText;
+    public GameObject blinkObject;
+
+
+    void Start()
+    {
+        mText = gameObject.GetComponent<TextMeshProUGUI>();
+        blinkText = blinkObject.GetComponent<TextMeshProUGUI>();
+        StartCoroutine(Blink());
+    }
+
+    void Update()
+    {
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                if (playerText.Length != 0)
+                {
+                    playerText = playerText.Substring(0, playerText.Length - 1);
+                }
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                CheckPassword();
+            }
+            else
+            {
+                playerText += c;
+            }
+        }
+        mText.text = playerText;
+    }
+
+    private IEnumerator Blink()
+    {
+        while(true)
+        {
+            blinkText.text = ">";
+            yield return new WaitForSeconds(.5f);
+            blinkText.text = "";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    public UnityEvent onWin;
+
+
+    void CheckPassword()
+    {
+        if (playerText.ToLower() == password)
+        {
+            onWin.Invoke();
+        }
+        else
+        {
+            playerText = "";
+        }
+    }
+}
