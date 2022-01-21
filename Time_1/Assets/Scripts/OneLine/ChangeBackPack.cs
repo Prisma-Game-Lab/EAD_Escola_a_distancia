1d16ae501743bf5fcbe95c39425b0df990874ec3
Nagib Moura Suaid <nagibmoura@gmail.com>
2022-01-21 11:59:01 -0300

    botão de mochila

--- Time_1/Assets/Scripts/OneLine/ChangeBackPack.cs
+++ Time_1/Assets/Scripts/OneLine/ChangeBackPack.cs
@@ -8,22 +8,36 @@ public class ChangeBackPack : MonoBehaviour
     [SerializeField]
     private Sprite clara;
     [SerializeField]
+    private Sprite claraOutline;
+
+    [SerializeField]
     private Sprite ericson;
+    [SerializeField]
+    private Sprite ericsonOutline;
+
     [SerializeField]
     private Sprite maria;
     [SerializeField]
+    private Sprite mariaOutline;
+
+    [SerializeField]
     private Image buttonImage;
+    [SerializeField]
+    private Image buttonOutline;
 
     private void Update() {
         switch (VariableManager.instance.activeCharacter) {
             case Student.clara:
                 buttonImage.sprite = clara;
+                buttonOutline.sprite = claraOutline;
                 break;
             case Student.ericson:
                 buttonImage.sprite = ericson;
+                buttonOutline.sprite = ericsonOutline;
                 break;
             case Student.maria:
                 buttonImage.sprite = maria;
+                buttonOutline.sprite = mariaOutline;
                 break;
         }
     }
