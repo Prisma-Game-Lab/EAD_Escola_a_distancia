c8309f0d52c821cc9fb4af0dc0e8b7ef5c1714db
rafaelrc7 <rafaelrc7@gmail.com>
2022-01-21 23:12:59 -0300

    Fixed build: Excluded editor only code

--- Time_1/Assets/Scripts/ResetVariables.cs
+++ Time_1/Assets/Scripts/ResetVariables.cs
@@ -1,7 +1,10 @@
 using UnityEditor;
 using UnityEngine;
 
+#if (UNITY_EDITOR)
 public class ResetVariables : MonoBehaviour {
     [MenuItem("Custom/Reset Variables")]
     static void ResetVariableManager() => VariableManager.instance.ResetVariables();
 }
+#endif
+
