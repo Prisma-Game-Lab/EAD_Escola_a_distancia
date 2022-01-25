4ffcb4dc6230a8f6a0855fa13658b7dcd71666e4
Thomaz Miranda <thomazrebellomiranda@gmail.com>
2022-01-25 11:39:07 -0300

    Fixed Description

--- Time_1/Assets/Scripts/ItemDescription.cs
+++ Time_1/Assets/Scripts/ItemDescription.cs
@@ -10,6 +10,33 @@ public class ItemDescription : MonoBehaviour
     public TextMeshProUGUI description;
 
     private void LateUpdate() {
-        transform.position = Input.mousePosition;
+        Vector3 mouse = Input.mousePosition;
+        if (mouse.x > Screen.width/2)
+        {
+            if (mouse.y > Screen.height/2)
+            {
+                //Debug.Log(2);
+                transform.position = new Vector3 (mouse.x - Screen.width/6, mouse.y - Screen.height/6, 0);
+            }
+            else
+            {
+                //Debug.Log(4);
+                transform.position = new Vector3 (mouse.x - Screen.width/6, mouse.y + Screen.height/6, 0);
+            }
+        }
+        else
+        {
+            if (mouse.y > Screen.height/2)
+            {
+                //Debug.Log(1);
+                transform.position = new Vector3 (mouse.x + Screen.width/6, mouse.y - Screen.height/6, 0);
+            }
+            else
+            {
+                //Debug.Log(3);
+                transform.position = new Vector3 (mouse.x + Screen.width/6, mouse.y + Screen.height/6, 0);
+            }
+        }
+        
     }
 }
